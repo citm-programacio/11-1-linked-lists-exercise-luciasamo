bd0314be3b5615b4630017d298befc45a0bd9730
luciasm1 <luciasm1@TR12-201-49>
2024-11-28 16:22:06 +0100

    me da un error externo

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -138,13 +138,13 @@ int main()
 	list.insert(0, 5);  // Insert at the beginning
 	list.print();
 
-	list.insert(2, 4);  // Insert at position 1
+	list.insert(2, 4);  // Insert at position 2
 	list.print();
 
-	list.insert(1, 6);  // Insert at position 2
+	list.insert(1, 6);  // Insert at position 1
 	list.print();
 
-	list.insert(8, 11); // Insert at position 3
+	list.insert(8, 11); // Insert at position 8
 	list.print();
 
 	return 0;
